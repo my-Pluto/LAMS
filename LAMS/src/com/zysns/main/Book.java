cd0ba63eb2c0e18150528349a5954c740d5b6dde
my-Pluto <1173171785@qq.com>
2019-06-19 11:36:45 +0800

    LAMS

--- LAMS/src/com/zysns/main/Book.java
+++ LAMS/src/com/zysns/main/Book.java
@@ -1,4 +1,96 @@
+/**
+ * 系统图书类
+ */
 package com.zysns.main;
 
+import java.util.Date;
+
 public class Book {
+    private String bno;  //图书编号
+    private String bname;    //图书名称
+    private String bauthor;    //图书作者
+    private String bpress;    //图书出版社
+    private Date bdate;    //图书出版时间
+    private String bisbn;    //图书ISBN
+    private String bbookno;   //图书所在书架编号
+    private int bquantity;    //图书馆藏数量
+
+    public Book() { }    //无参构造
+
+    public Book(String bno, String bname, String bauthor, String bpress, Date bdate, String bisbn, String bbookno, int bquantity) {  //有参构造
+        this.bno = bno;
+        this.bname = bname;
+        this.bauthor = bauthor;
+        this.bpress = bpress;
+        this.bdate = bdate;
+        this.bisbn = bisbn;
+        this.bbookno = bbookno;
+        this.bquantity = bquantity;
+    }
+
+    /*getter、setter函数*/
+
+    public String getBno() {
+        return bno;
+    }
+
+    public void setBno(String bno) {
+        this.bno = bno;
+    }
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+
+    public String getBauthor() {
+        return bauthor;
+    }
+
+    public void setBauthor(String bauthor) {
+        this.bauthor = bauthor;
+    }
+
+    public String getBpress() {
+        return bpress;
+    }
+
+    public void setBpress(String bpress) {
+        this.bpress = bpress;
+    }
+
+    public Date getBdate() {
+        return bdate;
+    }
+
+    public void setBdate(Date bdate) {
+        this.bdate = bdate;
+    }
+
+    public String getBisbn() {
+        return bisbn;
+    }
+
+    public void setBisbn(String bisbn) {
+        this.bisbn = bisbn;
+    }
+
+    public String getBbookno() {
+        return bbookno;
+    }
+
+    public void setBbookno(String bbookno) {
+        this.bbookno = bbookno;
+    }
+
+    public int getBquantity() {
+        return bquantity;
+    }
+
+    public void setBquantity(int bquantity) {
+        this.bquantity = bquantity;
+    }
 }
--- LAMS/src/com/zysns/main/Manager.java
+++ LAMS/src/com/zysns/main/Manager.java
@@ -1,4 +1,92 @@
 package com.zysns.main;
 
+import java.util.Date;
+
 public class Manager {
+    private double mno;  //管理员工号
+    private String mname;    //管理员姓名
+    private String mpassword;    //管理员密码
+    private String msex;    //管理员性别
+    private Date mdirthday;    //管理员出生日期
+    private String mdept;    //管理员所属部门
+    private double mlead;    //管理员领导
+    private int mlevel;    //管理员等级
+
+    public Manager() {
+    }
+
+    public Manager(double mno, String mname, String mpassword, String msex, Date mdirthday, String mdept, double mlead, int mlevel) {
+        this.mno = mno;
+        this.mname = mname;
+        this.mpassword = mpassword;
+        this.msex = msex;
+        this.mdirthday = mdirthday;
+        this.mdept = mdept;
+        this.mlead = mlead;
+        this.mlevel = mlevel;
+    }
+
+    public double getMno() {
+        return mno;
+    }
+
+    public void setMno(double mno) {
+        this.mno = mno;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public String getMpassword() {
+        return mpassword;
+    }
+
+    public void setMpassword(String mpassword) {
+        this.mpassword = mpassword;
+    }
+
+    public String getMsex() {
+        return msex;
+    }
+
+    public void setMsex(String msex) {
+        this.msex = msex;
+    }
+
+    public Date getMdirthday() {
+        return mdirthday;
+    }
+
+    public void setMdirthday(Date mdirthday) {
+        this.mdirthday = mdirthday;
+    }
+
+    public String getMdept() {
+        return mdept;
+    }
+
+    public void setMdept(String mdept) {
+        this.mdept = mdept;
+    }
+
+    public double getMlead() {
+        return mlead;
+    }
+
+    public void setMlead(double mlead) {
+        this.mlead = mlead;
+    }
+
+    public int getMlevel() {
+        return mlevel;
+    }
+
+    public void setMlevel(int mlevel) {
+        this.mlevel = mlevel;
+    }
 }
