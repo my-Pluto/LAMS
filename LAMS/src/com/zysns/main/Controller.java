3cce612760d32c23edd71bf558ae02574cfd8335
my-Pluto <1173171785@qq.com>
2019-06-20 08:22:40 +0800

    LAMS

--- LAMS/src/com/zysns/main/Controller.java
+++ LAMS/src/com/zysns/main/Controller.java
@@ -1,4 +1,17 @@
 package com.zysns.main;
 
+import com.zysns.other.About;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import com.zysns.other.*;
 public class Controller {
+
+    @FXML
+    private Button check;
+
+    @FXML
+    void check() {
+        About.showabout();
+    }
+
 }
